ae1c5ca3ccd0dd24ad1f04035d2b899b68fc8d2b
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-04 21:48:40 +1000

    feat: pool model

--- src/models/PoolModel.js
+++ src/models/PoolModel.js
@@ -3,6 +3,16 @@ const mongoose =require('mongoose')
 const Schema = mongoose.Schema;
 
 const PoolSchema = new Schema({
+    poolName:{
+        type: String,
+        required: true,
+    },
+    laneNumnber:{
+        type: Number,
+        required: true,
+        min: 1,
+        max:6
+    }
 
 
 });
