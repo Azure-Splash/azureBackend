7c00084f6038dd7e5df47d282daf31374f47b5d8
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-05 14:11:08 +1100

    feat: start bookings model

--- src/models/BookingModel.js
+++ src/models/BookingModel.js
@@ -3,6 +3,17 @@ const mongoose =require('mongoose')
 const Schema = mongoose.Schema;
 
 const BookingSchema = new Schema({
+   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+   pool: { type: mongoose.Schema.Types.ObjectId, ref: "Pool", required: true },
+   date: {
+      type: Date,
+      required: true,
+      unquie: true
+   }
+
+
+
+
 
 
 });
