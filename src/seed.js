f371d03e87cae644e38ceed7a2ca0d020cc930ed
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-07 08:51:52 +1000

    feat: seed file

--- src/seed.js
+++ src/seed.js
@@ -1,1 +1,30 @@
 require('dotenv').config()
+
+const mongoose = require('mongoose');
+const { databaseConnect } = require('./database');
+const { User } = require('./models/UserModel');
+
+databaseConnect().then(async () => {
+
+	console.log("Creating seed data!");
+
+
+    let testUser = new User({
+
+        firstName: "Scott",
+        lastName: "Jones",
+        email: "scott@email.com",
+        password: "password",
+        dateOfBirth: "24 november",
+        address: "123 fake st"
+    })
+
+    await testUser.save().then(() => {
+		console.log(`${testUser.firstName} is in the DB`);
+	});
+
+
+}).then(async () => {
+	// await dbDisconnect();
+})
+
--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request,response)=>{
     response.json({
         message:"Azure Splash"
     })
 });
+
+const { User } = require('./models/UserModel');
 
 module.exports={
     app
