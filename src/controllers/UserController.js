291e557de3850df7d7df24a78c2ac53c055f5899
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-08 00:39:13 +1000

    feat: test controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const { User } = require('../models/UserModel');
 const router = express.Router();
+
+router.get("/all", async (request, response) => {
+	// Empty object in .find() means get ALL documents
+	let result = await User.find({});
+
+	response.json({
+		users: result
+	});
+
+});
