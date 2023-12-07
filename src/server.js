5c765c81b6b0baf0083bf185b2819b1079b7ae1c
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-08 01:27:00 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express();
 
-const cors = require('cors');
-const corsOptions = {
-	//			frontend localhost,  frontend deployed
-	origin: ["http://localhost:3000/","http://localhost:3000", "https://someDeployedWebsite.com"],
-	optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+// const cors = require('cors');
+// const corsOptions = {
+// 	//			frontend localhost,  frontend deployed
+// 	origin: ["http://localhost:3000/","http://localhost:3000", "https://someDeployedWebsite.com"],
+// 	optionsSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get("/", (request,response)=>{
     response.json({
