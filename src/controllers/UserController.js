675296e90f49e173764fcaf0d2e39afc99ea4f08
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-11 12:30:25 +1100

    feat: user controller patch and delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,13 +13,13 @@ router.get("/all", async (request, response) => {
 });
 
 // find one user by id
-// router.get("/one/:id", async (request, response) => {
-// 	let result = await User.findOne({_id: request.params.id});
+router.get("/one/:id", async (request, response) => {
+	let result = await User.findOne({_id: request.params.id});
 
-// 	response.json({
-// 		user: result
-// 	});
-// });
+	response.json({
+		user: result
+	});
+});
 
 // find user by last name
 router.get("/name/:lastName", async (request, response) => {
@@ -36,28 +36,60 @@ router.get("/name/:lastName", async (request, response) => {
 router.post("/", async (request, response) => {
 	let newUser = await User.create(request.body).catch(error => error);
 
-	
-
     response.json(newUser);
 });
+
+
+// POST localhost:3000/users/login
+// router.post("/login", async (request, response) => {
+// 	// Find user by provided username 
+// 	let targetUser = await User.findOne({username: request.body.username}).catch(error => error);
+
+// 	// Check if user provided the correct password
+// 	let isPasswordCorrect = await comparePassword(request.body.password, targetUser.password);
+
+// 	if (!isPasswordCorrect){
+// 		response.status(403).json({error:"You are not authorised to do this!"});
+// 	}
+
+// 	// If they provided the correct, generate a JWT
+// 	let freshJwt = generateJwt(targetUser._id.toString());
+
+// 	// respond with the JWT 
+// 	response.json({
+// 		jwt: freshJwt
+// 	});
+
+// });
 
 // update user
 // does not overwrite or remove any unmentioned properties
 router.patch("/:id", async (request, response) => {
-	let result = null;
+	let result = await User.findByIdAndUpdate(
+		request.params.id, 
+		request.body,
+		{
+			returnDocument:"after",
+	
+		}
+		).catch(error => error);
+
+	response.json({
+		sighting: result
+	});
+
+});
 
 	response.json({
 		user: result
 	});
 
-});
-
 // delete user by id
 router.delete("/:id", async (request, response) => {
-	let result = null;
+	let result = await User.findByIdAndDelete(request.params.id).populate('user', '-password');
 
 	response.json({
-		user: result
+		deletedUser: result
 	});
 
 });
