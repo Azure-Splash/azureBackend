1438ee373347ec12a9f9d34a8693a52b29bbe349
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-11 19:54:57 +1100

    feat: trial find by user

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { Booking } = require('../models/BookingModel');
 const router = express.Router();
 
+
+// get all booking by id
 router.get("/admin/all", async (request, response) => {
 	// Empty object in .find() means get ALL documents
 	let result = await Booking.find({});
@@ -21,6 +23,32 @@ router.get("/admin/one/:id", async (request, response) => {
 	});
 });
 
+// get bookings by user id
+router.get("/admin/user/:id", async (request, response) =>{
+    let result = await Booking.find({ user: userId }).populate('user').populate('pool');
+
+    response.json({
+		booking: result
+	});
+});
+
+// edit booking by id
+router.patch("/:id", async (request, response) => {
+	let result = await Booking.findByIdAndUpdate(
+		request.params.id, 
+		request.body,
+		{
+			returnDocument:"after",
+	
+		}
+		).catch(error => error);
+
+	response.json({
+		booking: result
+	});
+
+});
+
 
 
 // delete booking by id
@@ -35,3 +63,21 @@ router.delete("/:id", async (request, response) => {
 
 
 module.exports = router;
+
+
+
+
+// // Controller function to get bookings by user
+// const getBookingsByUser = async (req, res) => {
+//   const userId = req.params.userId; // Assuming the user ID is passed as a parameter
+
+//   try {
+//     const bookings = await Booking.find({ user: userId }).populate('user').populate('pool').catch(error => error);
+//     res.json(bookings);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// };
+
+// module.exports = { getBookingsByUser };
