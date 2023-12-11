bf280c95100eb4f2ebdf74defba95c1395371010
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-11 14:02:56 +1100

    feat: CRUD on pool controller

--- src/controllers/PoolController.js
+++ src/controllers/PoolController.js
@@ -7,18 +7,46 @@ router.get("/all", async (request, response) => {
 	let result = await Pool.find({});
 
 	response.json({
-		staff: result
+		pool: result
 	});
 
 });
 
-// find one worker by id
+// find one pool by id
 router.get("/one/:id", async (request, response) => {
 	let result = await Pool.findOne({_id: request.params.id});
 
 	response.json({
-		staff: result
+		pool: result
 	});
+});
+
+// edit pool by id
+router.patch("/:id", async (request, response) => {
+	let result = await Pool.findByIdAndUpdate(
+		request.params.id, 
+		request.body,
+		{
+			returnDocument:"after",
+	
+		}
+		).catch(error => error);
+
+	response.json({
+		pool: result
+	});
+
+});
+
+
+// delete pool by id
+router.delete("/:id", async (request, response) => {
+	let result = await Pool.findByIdAndDelete(request.params.id).catch(error => error);
+
+	response.json({
+		deletedPool: result
+	});
+
 });
 
 module.exports = router;
