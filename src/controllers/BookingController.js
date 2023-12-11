07c2e6302e00e0a794c6b68e6f40f67bd3933c13
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-11 18:08:31 +1100

    feat: Booking CRUD

--- src/controllers/BookingController.js
+++ src/controllers/BookingController.js
@@ -14,11 +14,22 @@ router.get("/all", async (request, response) => {
 
 // find one booking by id
 router.get("/one/:id", async (request, response) => {
-	let result = await Booking.findOne({_id: request.params.id});
+	let result = await Booking.findOne({_id: request.params.id}).populate('user pool', '-password');
 
 	response.json({
 		booking: result
 	});
+});
+
+
+// delete booking by id
+router.delete("/:id", async (request, response) => {
+	let result = await Booking.findByIdAndDelete(request.params.id).catch(error => error);
+
+	response.json({
+		deletedBooking: result
+	});
+
 });
 
 
