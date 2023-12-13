95b6f6c144bb457e4ad57027df90101c7d34b8e2
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-13 22:27:46 +1000

    feat: admin only

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,9 +13,15 @@ router.get("/all", async (request, response) => {
 
 });
 
+
+
 // find one user by id
-router.get("/one/:id", async (request, response) => {
-	let result = await User.findOne({_id: request.params.id});
+// workers only
+router.get("/one/:id",authenticate, async (request, response) => {
+	if (!req.worker.isAdmin) {
+		return res.status(403).json({ message: "Unauthorized" });
+	  }
+	let result = await User.findOne({_id: request.params.id}).populate('-password');
 
 	response.json({
 		user: result
