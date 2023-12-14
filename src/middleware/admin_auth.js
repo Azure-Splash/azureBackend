ea57026b8396c850a0d8f73c33ce200b8ed9832e
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-15 08:55:48 +1000

    Update admin_auth.js

--- src/middleware/admin_auth.js
+++ src/middleware/admin_auth.js
@@ -3,7 +3,7 @@ const {Worker}  = require('../models/WorkersModel.js');
 
 // Middleware to check if the user is an admin
 const authAdmin = async (request, response, next) => {
-    const workerId = request.worker.id; // Assuming you have middleware to set req.user with the authenticated user
+    const workerId = request.worker.isAdmin; // Assuming you have middleware to set req.user with the authenticated user
   
     try {
       const worker = await Worker.findById(workerId);
