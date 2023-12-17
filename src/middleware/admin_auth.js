027e5d8dac2b12b75045f17fc8e098a2d07dc8eb
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-17 23:43:02 +1000

    fix: auth passport route

--- src/middleware/admin_auth.js
+++ src/middleware/admin_auth.js
@@ -1,27 +1,27 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-passport.use(
-  new LocalStrategy(async (email, password, done) => {
-    try {
-      const user = await user.findOne({ email });
-      if (!user) {
-        return done(null, false, { message: 'Incorrect email.' });
-      }
-      const isValidPassword = await user.isValidPassword(password);
-      if (!isValidPassword) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      const role = await Role.findOne({ name: user.role });
-      if (!role) {
-        return done(null, false, { message: 'Invalid role.' });
-      }
-      return done(null, user, { role: role });
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
+// passport.use(
+//   new LocalStrategy(async (email, password, done) => {
+//     try {
+//       const user = await user.findOne({ email });
+//       if (!user) {
+//         return done(null, false, { message: 'Incorrect email.' });
+//       }
+//       const isValidPassword = await user.isValidPassword(password);
+//       if (!isValidPassword) {
+//         return done(null, false, { message: 'Incorrect password.' });
+//       }
+//       const role = await Role.findOne({ name: user.role });
+//       if (!role) {
+//         return done(null, false, { message: 'Invalid role.' });
+//       }
+//       return done(null, user, { role: role });
+//     } catch (err) {
+//       return done(err);
+//     }
+//   })
+// );
 
 const isAdmin = (request, response, next) => {
   if (request.user.role.name === 'admin') {
