34d984e6d1a60e416fec75404edd2884780d12c6
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-18 10:27:31 +1000

    Update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -10,15 +10,15 @@ const LocalStrategy = require('passport-local').Strategy;
 passport.use(
 	new LocalStrategy(async (email, password, done) => {
 	  try {
-		const user = await user.findOne({ email });
+		const user = await User.findOne({ email });
 		if (!user) {
 		  return done(null, false, { message: 'Incorrect email.' });
 		}
-		const isValidPassword = await user.isValidPassword(password);
+		const isValidPassword = await User.isValidPassword(password);
 		if (!isValidPassword) {
 		  return done(null, false, { message: 'Incorrect password.' });
 		}
-		const role = await role.findOne({ name: user.role.name });
+		const role = await Role.findOne({ name: user.role });
 		if (!role) {
 		  return done(null, false, { message: 'Invalid role.' });
 		}
