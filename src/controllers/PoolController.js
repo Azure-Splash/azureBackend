a7b8945f3a2a0b72f26b11b8a79b399cb4616bc3
Fabian <128451994+fabs-pe@users.noreply.github.com>
2024-01-02 20:48:57 +1000

    feat: pool routes completed (#7)

--- src/controllers/PoolController.js
+++ src/controllers/PoolController.js
@@ -1,56 +1,86 @@
 const express = require('express');
 const { Pool } = require('../models/PoolModel');
+const { authUser } = require('../middleware/admin_auth');
 const router = express.Router();
 
 
 // list all pools
 // admin only
-router.get("/pools/all", async (request, response) => {
-	
-	let result = await Pool.find({});
+router.get("/admin/all", authUser, async (request, response) => {
+	if (request.user.role === 'admin'){
+		let result = await Pool.find({});
 
-	response.json({
-		pool: result
-	});
-
+		response.json({pool: result});
+	} else{
+		response.status(403).json({error: 'Access Forbidden'})
+	}
 });
 
 // find one pool by id
-//admin only
-router.get("admin//one/:id", async (request, response) => {
-	let result = await Pool.findOne({_id: request.params.id});
+// admin only
+router.get("admin/one/:id", authUser, async (request, response) => {
+	if (request.user.role === 'admin'){
+		let result = await Pool.findOne({_id: request.params.id});
 
-	response.json({
-		pool: result
-	});
+		response.json({pool: result});
+
+	} else{
+		response.status(403).json({error: 'Access Forbidden'})
+	}
 });
 
+//create a new pool
+// admin only
+router.post("/admin/new", authUser, async (request, response) =>{
+	if(request.user.role === 'admin'){
+	
+		let newPool = await Pool.create(request.body).catch(error => error);
+
+		response.status(201).json({message: 'Pool created successfully!', pool: newPool});
+	
+	}
+});
+
+
 // edit pool by id
-router.patch("/:id", async (request, response) => {
+// admin only
+router.patch("/admin/update/:id", authUser, async (request, response) => {
+	if (request.user.role === 'admin'){
+
+		
 	let result = await Pool.findByIdAndUpdate(
 		request.params.id, 
 		request.body,
 		{
-			returnDocument:"after",
+			returnDocument:"after", 
 	
 		}
-		).catch(error => error);
+		);
+		
+		response.json({pool: result});
 
-	response.json({
-		pool: result
-	});
-
+	} else{
+		response.status(403).json({error: 'Access Forbidden'})
+	}
 });
 
 
 // delete pool by id
-router.delete("/:id", async (request, response) => {
-	let result = await Pool.findByIdAndDelete(request.params.id).catch(error => error);
-
-	response.json({
-		deletedPool: result
-	});
-
-});
+// admin only
+router.delete("/admin/delete/:id", authUser, async (request, response) => {
+	if (request.user.role === 'admin'){
+		let result = await Pool.findByIdAndDelete(request.params.id)
+	try {
+	  if (!result) {
+		return response.status(400).json({ message: "Pool not found or could not be deleted" });
+	  }
+  
+	  response.json({ message: "Pool deleted successfully" });
+	} catch (error) {
+		console.error(error);
+	  	response.status(500).json({ message: "An error occurred while trying to delete the pool" });
+	}
+}
+  });
 
 module.exports = router;
