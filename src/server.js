b4375dbd6b6a9df1cc23910e916a7f09f3a0d97d
Fabian <128451994+fabs-pe@users.noreply.github.com>
2024-01-06 20:11:56 +1000

    cors fix

--- src/server.js
+++ src/server.js
@@ -32,11 +32,11 @@ app.get("/", (request,response)=>{
 });
 
 // enable cors
-app.use(function(_, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(_, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // add controllers
 
