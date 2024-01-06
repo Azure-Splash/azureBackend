01ef5d803582937bb927113a3d716deca2e6d447
Fabian <128451994+fabs-pe@users.noreply.github.com>
2024-01-06 20:03:58 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const cors = require('cors');
 
 
 const corsOptions = {
-	origin: ["http://localhost:3000/","http://localhost:3005", "https://azuresplash.netlify.app/"],
+	origin: ["http://localhost:3000","http://localhost:3005", "https://azuresplash.netlify.app/"],
 	optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
