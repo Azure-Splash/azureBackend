dcb087afba435e60536971e95f8b855462a914be
Fabian <128451994+fabs-pe@users.noreply.github.com>
2024-01-07 12:54:20 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,22 +3,32 @@ const app = express();
 const cors = require('cors');
 
 
+const helmet = require("helmet");
+app.use(helmet());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+    },
+  })
+);
+const corsOptions = {
+	origin: ["http://localhost:3000/","http://localhost:3005", "https://azuresplash.netlify.app/"],
+	optionsSuccessStatus: 200 
+}
 
-// const corsOptions = {
-// 	origin: ["http://localhost:3000/","http://localhost:3005", "https://azuresplash.netlify.app/"],
-// 	optionsSuccessStatus: 200 
-// }
-
-// app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
-
-app.use(cors({
-    origin: ["http://localhost:3000/","http://localhost:3005", "https://azuresplash.netlify.app/"],
-    methods: ['POST', 'GET', 'OPTIONS', 'DELETE'],
-    credentials: true,
-  }));
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
+
+// app.use(cors({
+//     origin: ["http://localhost:3000/","http://localhost:3005", "https://azuresplash.netlify.app/"],
+//     methods: ['POST', 'GET', 'OPTIONS', 'DELETE'],
+//     credentials: true,
+//   }));
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 // Configure API data receiving & sending
 app.use(express.json());
