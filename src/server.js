9af7d40721b400884c14a688dfd9771bd6325210
Fabian <128451994+fabs-pe@users.noreply.github.com>
2024-01-07 19:57:41 +1000

    Update server.js

--- src/server.js
+++ src/server.js
@@ -24,10 +24,10 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// Enable CORS
 app.use(function(_, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true"); // Set 'Access-Control-Allow-Credentials' to 'true'
     next();
 });
 
